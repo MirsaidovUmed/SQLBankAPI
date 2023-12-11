34511a8394aa858b7451a1240de7b427c2b2a931
MirsaidovUmed <umedmirsaidov61@gmail.com>
2023-12-11 22:58:46 +0500

    changed the project format

---
+++ pkg/database.go
@@ -0,0 +1,5 @@
+package pkg
+
+var Database = make(map[string]float64)
+
+var Percent = 10.0
---
+++ pkg/registerClient.go
@@ -0,0 +1,12 @@
+package pkg
+
+import "fmt"
+
+func RegisterClient() {
+	var (
+		name string
+	)
+	fmt.Println("Введите имя клиента")
+	fmt.Scan(&name)
+	Database[name] = 0
+}
